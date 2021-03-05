b5f2e0a18963b5fd8f274724c92742ff19983f83
Juanse Gómez <js.gomez14@uniandes.edu.co>
2021-03-04 21:50:26 -0500

    Basic Navigation

--- app/src/main/java/com/example/basicapp/MainActivity.java
+++ app/src/main/java/com/example/basicapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.basicapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createEntry(View view) {
+        Intent intent = new Intent(MainActivity.this, CreateEntry.class);
+        startActivity(intent);
+    }
+
+    public void createOut(View view) {
+        Intent intent = new Intent(MainActivity.this, CreateOut.class);
+        startActivity(intent);
+    }
+
+    public void viewEntries(View view) {
+        Intent intent = new Intent(MainActivity.this, ViewEntriesActivity.class);
+        startActivity(intent);
+    }
+
+    public void viewOuts(View view) {
+        Intent intent = new Intent(MainActivity.this, ViewOutsActivity.class);
+        startActivity(intent);
     }
 }
