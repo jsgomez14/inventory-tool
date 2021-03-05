c5fdfff71118f74687896992b34bbcaec13f67b4
Juanse Gómez <js.gomez14@uniandes.edu.co>
2021-03-04 22:59:16 -0500

    Mongo Dependencies setup

--- app/src/main/java/com/example/basicapp/MainActivity.java
+++ app/src/main/java/com/example/basicapp/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.basicapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Credentials;
 import android.os.Bundle;
 import android.view.View;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MongoClientURI uri = new MongoClientURI("");
+
+        MongoClient mongoClient = new MongoClient(uri);
+        MongoDatabase database = mongoClient.getDatabase("inventory");
+
+        MongoCollection<Document> collection = database.getCollection("stock_summary");
+
+        Document queryFilter  = new Document("type", "perennial");
     }
 
     public void createEntry(View view) {
